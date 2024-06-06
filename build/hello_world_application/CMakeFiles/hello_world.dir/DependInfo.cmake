1176d130311a186e3c57c9df7e84ff8a0ca4e55c
socl0sed <letuchiy.force@gmail.com>
2024-06-06 05:56:46 +0000

    work

---
+++ build/hello_world_application/CMakeFiles/hello_world.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/andrew/AndrewB203/workspacenew/lab03/hello_world_application/hello_world.cpp" "hello_world_application/CMakeFiles/hello_world.dir/hello_world.cpp.o" "gcc" "hello_world_application/CMakeFiles/hello_world.dir/hello_world.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/andrew/AndrewB203/workspacenew/lab03/build/formatter_ex_lib/CMakeFiles/formatter_ex_lib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
